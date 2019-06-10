af7311e046204905b46e2aff04eff974262780ea
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-06-10 11:18:08 +0100

    set state with nested hashes and timer

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,51 @@
-// Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }    
+        }
+    }
+
+    updateBitrate = () => {
+        this.setState({
+            settings: {
+              ...this.state.settings,
+              bitrate: 12
+            }
+          })
+    }
+
+    updateResolution = () => {
+        this.setState(prevState => ({
+            ...prevState,
+            settings: {
+                ...prevState.settings,
+                video: {
+                    ...prevState.settings.video, 
+                    resolution: '720p'
+                }
+            }
+        }))
+    }
+
+   
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.updateBitrate} className="bitrate">Change Bitrate</button>
+                <button onClick={this.updateResolution} className="resolution">Change Resolution</button>
+            </div>
+        )
+    }
+}
